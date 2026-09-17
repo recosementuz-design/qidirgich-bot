bfc81c27935d989b326f133da4a09c166c886eac
recosementuz-design <recosement.uz@gmail.com>
2026-09-17 08:29:57 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN topilmadi")
-
-if not API_ID:
-    raise RuntimeError("API_ID topilmadi")
-
-if not API_HASH:
-    raise RuntimeError("API_HASH topilmadi")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL topilmadi")
-
-API_ID = int(API_ID)
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+DATABASE_URL = os.environ["DATABASE_URL"]
+STRING_SESSION = os.environ.get("STRING_SESSION", "").strip()
+ADMIN_TELEGRAM_ID = int(os.environ["ADMIN_TELEGRAM_ID"])
