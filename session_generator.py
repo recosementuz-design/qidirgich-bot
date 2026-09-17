e2b78bec3f96db83617d96adda0b6b41f0a6a930
recosementuz-design <recosement.uz@gmail.com>
2026-09-17 08:52:15 +0500

    Update session_generator.py

--- session_generator.py
+++ session_generator.py
@@ -1,32 +1,63 @@
 import asyncio
-from telethon import TelegramClient
+import os
 
-API_ID = 5923818664
-API_HASH = "8f6f39d739c367dea66fbe3a137aff0a"
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+
+API_ID = int(
+    os.environ.get(
+        "API_ID"
+    )
+    or input("API_ID: ").strip()
+)
+
+API_HASH = (
+    os.environ.get(
+        "API_HASH"
+    )
+    or input("API_HASH: ").strip()
+)
+
 
 async def main():
+
     client = TelegramClient(
-        "qidirgich_session",
+        StringSession(),
         API_ID,
         API_HASH
     )
 
     await client.start()
 
-    me = await client.get_me()
+    session = client.session.save()
 
     print()
-    print("================================")
-    print("SESSION MUVAFFAQIYATLI YARATILDI")
-    print("================================")
-    print("ID:", me.id)
-    print("Username:", getattr(me, "username", None))
-    print("Session fayli:")
-    print("qidirgich_session.session")
-    print()
+    print(
+        "================================"
+    )
+    print(
+        "STRING_SESSION"
+    )
+    print(
+        "================================"
+    )
+    print(session)
+    print(
+        "================================"
+    )
+    print(
+        "END"
+    )
+    print(
+        "================================"
+    )
 
     await client.disconnect()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+
+    asyncio.run(
+        main()
+    )
